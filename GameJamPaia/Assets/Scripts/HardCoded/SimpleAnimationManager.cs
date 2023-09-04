9d6d738e04bac4e68be114cd95f5adf4f9016556
DESKTOP-5FVF2CB\abnne <abnner.urzedo@gmail.com>
2023-09-04 14:52:42 -0300

    walk e idle

---
+++ GameJamPaia/Assets/Scripts/HardCoded/SimpleAnimationManager.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+
+public class SimpleAnimationManager : MonoBehaviour
+{
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private Animator anim;
+    [SerializeField] private string walkAnimation;
+    [SerializeField] private string idleAnimation;
+
+    private void Awake()
+    {
+        if(inputManager ==null)
+            inputManager = FindObjectOfType<InputManager>();
+
+        inputManager.OnMoveInput += OnMoveInput;
+
+        anim.Play(idleAnimation, 0, 0);
+
+    }
+
+    private void OnMoveInput(Vector2 vector)
+    {
+        if (vector == Vector2.zero)
+            anim.Play(idleAnimation, 0, 0);
+        else
+            anim.Play(walkAnimation, 0, 0);
+    }
+}
